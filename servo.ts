dd4e3275bfde4354dff31ac99e46e81ba4bd05d9
noqman <140384051+noqman@users.noreply.github.com>
2024-12-12 21:57:35 +0800

    Optimized Servo Blocks

--- servo.ts
+++ servo.ts
@@ -56,7 +56,7 @@ namespace sumobit{
      * Set the position for servo (0-180 degrees).
      * @param servo The servo to control. eg: SumobitServoChannel.Servo1
      * @param position The desired angle of the servo. eg: 90
-     * @param speed Servo movement speed. Higher values result in faster movement. eg:9
+     * @param speed Servo movement speed. Higher values result in faster movement. eg:5
      */
     //% group="Servos"
     //% weight=39
@@ -66,27 +66,27 @@ namespace sumobit{
     //% servo.defl= SumobitServoChannel.Servo1
     //% position.min=0 position.max=180
     //% position.defl=90
-    //% speed.min=0 speed.max=180
-    //% speed.defl=9
-    export function setServoPosition(servo: SumobitServoChannel, position: number, speed:number=9): void {
+    //% speed.min=1 speed.max=5
+    //% speed.defl=5
+    export function setServoPosition(servo: SumobitServoChannel, position: number, speed:number=5): void {
         position = sumobit.limit(position, 0, 180);
 
         switch (servo) {
             case SumobitServoChannel.Servo1:
-                sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV1_POS, position);
                 sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV1_SPEED, speed);
+                sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV1_POS, position);
                 break;
 
             case SumobitServoChannel.Servo2:
-                sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV2_POS, position);
                 sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV2_SPEED, speed);
+                sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV2_POS, position);
                 break;
 
             case SumobitServoChannel.All:
-                sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV1_POS, position);
-                sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV2_POS, position);
                 sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV1_SPEED, speed);
                 sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV2_SPEED, speed);
+                sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV1_POS, position);
+                sumobit.i2cWrite(SUMOBIT_REG_ADD_SRV2_POS, position);
                 break;
         }
     }
